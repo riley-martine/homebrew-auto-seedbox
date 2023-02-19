b0da2570467ea2d879296ce7b3d45f782bf3d267
Riley Martine <riley.martine@protonmail.com>
2023-02-19 13:17:10 -0700

    Initial commit of files

---
+++ auto_download/torrent_name.py
@@ -0,0 +1,24 @@
+#!/opt/homebrew/bin/python3.11
+"""Print the 'name' metadata from a torrent file"""
+
+# Needs to be brew python3.11 for libtorrent bindings
+
+import pathlib
+import sys
+import warnings
+
+import libtorrent
+
+# libtorrent uses dunder methods for some things and we want clean output
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+if len(sys.argv) != 2:
+    print("Pass an argument.", file=sys.stderr)
+    sys.exit(1)
+
+torrent_file = pathlib.Path(sys.argv[1])
+if not torrent_file.is_file():
+    print("Cannot find file at: {torrent_file}", file=sys.stderr)
+    sys.exit(1)
+
+print(libtorrent.torrent_info(torrent_file.absolute()).name())
---
+++ auto_download/wait_and_download.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Wait for a specified torrent to be downloaded to the seedbox,
+# then download it to ~/Downloads with rclone
+set -euo pipefail
+
+# https://gist.github.com/sj26/88e1c6584397bb7c13bd11108a579746
+function retry {
+    local retries=$1
+    shift
+
+    local count=0
+    until "$@"; do
+        exit=$?
+        wait=$((2 ** count))
+        wait=$((wait < 30 ? wait : 30))
+        count=$((count + 1))
+        if [ "$count" -lt "$retries" ]; then
+            echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+            sleep $wait
+        else
+            echo "Retry $count/$retries exited $exit, no more retries left."
+            return $exit
+        fi
+    done
+    return 0
+}
+
+TORRENT_NAME="$("$GIT_ROOT"/auto_download/torrent_name.py "$1").torrent"
+
+function is_complete {
+    rclone lsjson seedbox:/home/"$SEEDBOX_USER"/completed_torrents |
+        jq -e --arg FILENAME "$TORRENT_NAME" '.[]|select(.Name == $FILENAME)' > /dev/null
+}
+
+echo "Waiting for $TORRENT_NAME to finish downloading to seedbox..."
+retry 240 is_complete "$TORRENT_NAME"
+echo "$TORRENT_NAME has completed download to seedbox. Downloading new torrents to local..."
+
+rclone --max-age 24h --no-traverse copy seedbox:/home/"$SEEDBOX_USER"/twatch_out/ ~/Downloads
+echo "Done downloading new torrents."
+
+function send_epubs {
+    set -x
+    for epub in $("$GIT_ROOT"/auto_kindle/get_torrent_epub_files.py "$1"); do
+        "$GIT_ROOT"/auto_kindle/copy_to_kindle.sh "$HOME/Downloads/$epub"
+    done
+    set +x
+}
+
+# Don't care that much if it fails
+# Which it might, if kindle is offline, which it often is!
+retry 20 send_epubs "$1" || echo "Failed to send epubs."
